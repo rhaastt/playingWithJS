8cd64d2856c10d88bfc51010bf3f9f9a21803b90
Luis Miguel <87282690+rhaastt@users.noreply.github.com>
2025-08-23 20:54:36 +0000

    criado o menu mas sem estilo bonito

--- classAverage.js
+++ classAverage.js
@@ -8,6 +8,9 @@ function getAverage(scores) {
   return sum / scores.length;
 }
 
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
 function getGrade(score) {
   if (score === 100) {
     return "A++";
